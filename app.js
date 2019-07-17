869e9a65fb490c0739f54f4115afbe4e25941c78
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2019-07-17 11:43:56 +0700

    video-10

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
     'mongodb+srv://saya:'+ 
@@ -15,6 +16,7 @@ mongoose.connect(
 );
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -36,6 +38,7 @@ app.use((req,res,next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req,res,next)=>{
     const error = new Error('Not found');
